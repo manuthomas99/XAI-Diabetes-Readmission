ddb5bcd668239507e8c1123d4ac7db3ac8d4302b
manuthomas99 <manuthomas233@gmail.com>
2026-02-25 16:13:20 +0100

    data loader initial

---
+++ main.py
@@ -0,0 +1,18 @@
+#Driver script for experiments.
+import argparse
+from utils.data_loader import DataLoader
+DATA_PATH = "/Users/manu/Desktop/Projects/XAI-Diabetes-Readmission/data/diabetes+130-us+hospitals+for+years+1999-2008/diabetic_data.csv"
+TARGET_COLUMN_NAME = "readmitted"
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run experiments with different configurations.")
+    # parser.add_argument("--data_path", type=str, required=True, help="Path to the dataset.")
+    parser.add_argument("--test_size", type=float, default=0.2, help="Proportion of the dataset to include in the test split.")
+    
+    args = parser.parse_args()
+    
+    # Initialize DataLoader with the provided data path and test size
+    data_loader = DataLoader(data_path=DATA_PATH, target_column_name=TARGET_COLUMN_NAME, test_size=args.test_size)
+    
---
+++ utils/data_loader.py
@@ -0,0 +1,30 @@
+import csv
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import random  
+class DataLoader:
+    def __init__(self, data_path,target_column_name, test_size=0.2):
+        self.train_data = None
+        self.test_data = None
+        self.data = None
+        self.target_column_name = target_column_name
+        self.data_path = data_path
+
+        self.load_data()
+        self.train_data, self.test_data = self.test_train_split(test_size=test_size)
+
+    def load_data(self):
+        # Loading entire data from csv
+        with open(self.data_path, "r") as file:
+            self.data = pd.read_csv(file)
+        return self.data
+    
+    def test_train_split(self, test_size=0.2):
+        # Split the data into train, test and validation sets
+        X = self.data.drop(self.target_column_name, axis=1) 
+        y = self.data[self.target_column_name]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random.randint(0, 2**32 - 1))
+        self.train_data = pd.concat([X_train, y_train], axis=1)
+        self.test_data = pd.concat([X_test, y_test], axis=1)
+        return self.train_data, self.test_data
